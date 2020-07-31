4cfe8f468d8cf403eb16631d8a3529937e3ef4ae
twber <hctsai@umich.edu>
2020-07-31 17:30:21 +0800

    Initial Commit

---
+++ silhouette/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  silhouette
+//
+//  Created by Bernice TSAI on 2020/7/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
